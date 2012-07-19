b60f642957a1ca85db9e9e9be224255ff0d9858b
gygounet@gmail.com <gygounet@gmail.com@7e88f350-4b46-e3a9-bb93-d555f044ff22>
2012-07-19 22:51:34 +0000

    Subo primera version de las reglas en una carpeta aparte, tengo un error horripilante.

---
+++ trunk/4- Implementacion/SoftballExpert/src/Lanzador.java
@@ -0,0 +1,68 @@
+
+public class Lanzador {
+	private String distPeso;
+	private String pies;
+	private String soltadoPelota;
+	private String movMunieca;
+	private String traza;
+	private String velocidad;
+	private String lanzamiento;
+	
+	public void setDistPeso(String distPeso) {
+		this.distPeso = distPeso;
+	}
+	
+	public String getDistPeso() {
+		return distPeso;
+	}
+	
+	public void setPies(String pies) {
+		this.pies = pies;
+	}
+	
+	public String getPies() {
+		return pies;
+	}
+	
+	public void setSoltadoPelota(String soltadoPelota) {
+		this.soltadoPelota = soltadoPelota;
+	}
+	
+	public String getSoltadoPelota() {
+		return soltadoPelota;
+	}
+	
+	public void setMovMunieca(String movMunieca) {
+		this.movMunieca = movMunieca;
+	}
+	
+	public String getMovMunieca() {
+		return movMunieca;
+	}
+	
+	public void setTraza(String traza) {
+		this.traza = traza;
+	}
+	
+	public String getTraza() {
+		return traza;
+	}
+	
+	public void setVelocidad(String velocidad) {
+		this.velocidad = velocidad;
+	}
+	
+	public String getVelocidad() {
+		return velocidad;
+	}
+
+	public void setLanzamiento(String lanzamiento) {
+		this.lanzamiento = lanzamiento;
+	}
+
+	public String getLanzamiento() {
+		return lanzamiento;
+	}
+	
+	
+}
---
+++ trunk/4- Implementacion/SoftballExpert/src/main.java
@@ -0,0 +1,17 @@
+
+public class main {
+
+	public static void main(String[]args) {
+		EvaluadorReglas ev = new EvaluadorReglas();
+		Lanzador lanzador = new Lanzador();
+		lanzador.setDistPeso("Medio");
+		lanzador.setPies("NoAlineado");
+		lanzador.setSoltadoPelota("Medio");
+		lanzador.setMovMunieca("Medio");
+		lanzador.setTraza("Recta");
+		lanzador.setVelocidad("Alta");
+		ev.evaluarReglas(lanzador);
+		System.out.println("Resultado Esperado : Fastball Resultado Obtenido: "+ lanzador.getLanzamiento());
+		
+	}
+}
