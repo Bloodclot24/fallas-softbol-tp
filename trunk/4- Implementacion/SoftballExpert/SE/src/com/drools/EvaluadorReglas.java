f04f19735c77bde199dc1366c188c2913d1b0742
gygounet@gmail.com <gygounet@gmail.com@7e88f350-4b46-e3a9-bb93-d555f044ff22>
2012-07-20 02:29:24 +0000

    version 2 con el drools correctamente instalado

---
+++ trunk/4- Implementacion/SoftballExpert/SE/src/com/drools/EvaluadorReglas.java
@@ -0,0 +1,60 @@
+package com.drools;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import org.drools.RuleBase;
+import org.drools.RuleBaseFactory;
+import org.drools.WorkingMemory;
+import org.drools.compiler.PackageBuilder;
+import org.drools.rule.Package; 
+	
+public class EvaluadorReglas {
+	 
+	    public String evaluarReglas(Lanzador lanzador) {
+	        try {
+	            //Cargamos la base de reglas
+	            RuleBase ruleBase = leerReglas();
+	            WorkingMemory workingMemory = ruleBase.newStatefulSession();
+	            workingMemory.insert(lanzador);
+	            
+	            //Disparamos las reglas de Drools
+	            workingMemory.fireAllRules();
+	            
+	            
+	 
+	        } catch (Throwable t) {
+	            t.printStackTrace();
+	        }
+	        return lanzador.getLanzamiento();
+            
+	    }
+	 
+	    
+	    private static RuleBase leerReglas() throws Exception {
+	        //Leemos el archivo de reglas (DRL)
+	        Reader source = new InputStreamReader(
+	            EvaluadorReglas.class.getResourceAsStream("reglasV1.drl"));
+	 
+	        //Construimos un paquete de reglas
+	        PackageBuilder builder = new PackageBuilder();
+	 
+	        //Parseamos y compilamos las reglas en un único paso
+	        builder.addPackageFromDrl(source);
+	 
+	        // Verificamos el builder para ver si hubo errores
+	        if (builder.hasErrors()) {
+	            System.out.println(builder.getErrors().toString());
+	            throw new RuntimeException(
+	                "No se pudo compilar el archivo de reglas.");
+	        }
+	 
+	        //Obtenemos el package de reglas compilado
+	        Package pkg = builder.getPackage();
+	 
+	        //Agregamos el paquete a la base de reglas 
+	        //(desplegamos el paquete de reglas).
+	        RuleBase ruleBase = RuleBaseFactory.newRuleBase();
+	        ruleBase.addPackage(pkg);
+	        return ruleBase;
+	    }
+}
+
